a489f9a6569b735a46829f22ea42352a9971d774
StefanoOr <Bastord@hotmail.it>
2015-11-22 23:17:24 +0100

    Update Registro.sql

--- Registro.sql
+++ Registro.sql
@@ -7,9 +7,9 @@ CREATE TABLE Studenti (ID int NOT NULL AUTO_INCREMENT,Cognome varchar (30), Nome
 
 INSERT INTO Studenti (ID,Cognome,Nome) VALUES   (NULL,'Antonelli','Nicola'),(NULL,'Bellanova','Alessandro'),(NULL,'Cabras','Manuel'),
                                                 (NULL,'Canetto','Fabio'),   (NULL,'Cantarelli','Roberto'),  (NULL,'Casti','Simone'),
-						                                    (NULL,'Concas','Emanuele'), (NULL,'Corda','Federico'),      (NULL,'Curreli','Stefano'),
-						                                    (NULL,'Fruttu','Luca'),     (NULL,'Lai','Roberto'),         (NULL,'Lilliu','Giacomo'),
-					                                      (NULL,'Marongiu','Mattia'), (NULL,'Murano','Luca'),         (NULL,'Orru','Stefano'),
-						                                    (NULL,'Monni','Gabriele');
+					        (NULL,'Concas','Emanuele'), (NULL,'Corda','Federico'),      (NULL,'Curreli','Stefano'),
+					        (NULL,'Fruttu','Luca'),     (NULL,'Lai','Roberto'),         (NULL,'Lilliu','Giacomo'),
+					        (NULL,'Marongiu','Mattia'), (NULL,'Murano','Luca'),         (NULL,'Orru','Stefano'),
+						(NULL,'Monni','Gabriele');
 												   
 												   
